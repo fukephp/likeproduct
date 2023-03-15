5e1cdb4db0abff0e08ef346e9fcc4aad441c41be
farukh <faruk.hopic@gmail.com>
2023-03-15 15:01:53 +0100

    Create Product - model, controller, factory Register api route products

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,10 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ProductFactory extends Factory
 {
+
+    protected $model = Product::class;
     /**
      * Define the model's default state.
      *
@@ -14,7 +18,9 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->text(30),
+            'image' => $this->faker->imageUrl(),
+            'user_id' => User::find(1)
         ];
     }
 }
