4638c19ced410899621b86959c50c537a1d27660
farukh <faruk.hopic@gmail.com>
2023-03-17 12:59:40 +0100

    Enable sqlite on testing Enable sanctum middleware

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,35 @@
 
 namespace Tests;
 
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function createUser(): User
+    {
+        return User::factory()->create();
+    }
+
+    public function createUserRaw(): array
+    {
+        return User::factory()->raw();
+    }
+
+    public function createProductForUser(User $user): Product
+    {
+        return Product::factory()
+            ->for($user)
+            ->create();
+    }
+
+    public function getProductForUserRaw(User $user): array
+    {
+        return Product::factory()
+            ->for($user)
+            ->raw();
+    }
 }
